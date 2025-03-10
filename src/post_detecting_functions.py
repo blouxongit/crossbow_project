295e28383d7dd4a4cc110aaa6edcde1e4b7a24b5
Blouxongit <mr.bloux@gmail.com>
2025-03-11 00:32:58 +0100

    feat: added postprocessing functions

---
+++ src/post_detecting_functions.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+# The value at index i contains a list of 2 elements:
+# - At index 0: The np.ndarray of the 2d coordinates in the left image
+# - At index 1: The np.ndarray of the 2d coordinates in the right image
+
+
+def noop(projectiles_coordinates_2d):
+    return projectiles_coordinates_2d
+
+
+def circle_detector_post_processing(projectiles_coordinates_2d):
+    left_detections = [detections[0] for detections in projectiles_coordinates_2d]
+    right_detections = [detections[1] for detections in projectiles_coordinates_2d]
+
+    left_cleaned_detections = circle_detector_post_processing_per_camera(left_detections)
+    right_cleaned_detections = circle_detector_post_processing_per_camera(right_detections)
+
+    cleaned_projectiles_coordinates_2d = [
+        [left_cleaned_detection, right_cleaned_detection]
+        for left_cleaned_detection, right_cleaned_detection in zip(left_cleaned_detections, right_cleaned_detections)
+    ]
+
+    return cleaned_projectiles_coordinates_2d
+
+
+def circle_detector_post_processing_per_camera(detections):
+    suspects = []
+    invalid_detections = []
+
+    for detection in detections:
+        if len(detection) > 1:
+            suspects.extend(detection)
+
+    for suspect in suspects:
+        if (
+            suspects.count(suspect) > 2
+        ):  # In theory, we could put 1, but we give a margin for error. We consider that if we get more than 2 times the exact same detection point, then it is not an actual detection point.
+            invalid_detections.append(suspect)
+
+    for invalid_detection in invalid_detections:
+        for detection in detections:
+            if invalid_detection in detection:
+                detection.remove(invalid_detection)
+
+    cleaned_detections = []
+    for detection in detections:
+        cleaned_detections.append(detection if len(detection) == 1 else [])
+
+    return cleaned_detections
