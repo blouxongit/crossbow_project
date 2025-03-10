e0e80c11362e02eef0252c30533f118410851251
Blouxongit <mr.bloux@gmail.com>
2025-03-11 00:32:29 +0100

    feat: added verification for 3d reconstruction

--- src/experience_manager.py
+++ src/experience_manager.py
@@ -8,11 +8,8 @@ from camera import CameraSetup
 from configuration_reader import Config
 from files_manager import FilesManager, read_image
 from image_processor import ColorDomain, ImagesPairProcessor
+from post_detecting_functions import noop
 from projectile_finder import AvailableProjectileFindersMethod
-
-
-def noop(x):
-    return x
 
 
 class ExperienceManager:
@@ -53,7 +50,7 @@ class ExperienceManager:
 
         self.images_pair_processor.set_camera_pair_images(left_image, right_image)
 
-    def extract_projectiles_2d_coordinates(self, extraction_postprocessing_function: callable = noop):
+    def extract_projectiles_2d_coordinates(self, post_detecting_function: callable = noop):
         images_sequence = self.files_manager.get_matching_images_path()
         for matching_images_pair_path in images_sequence:
             self.assign_images(matching_images_pair=matching_images_pair_path)
@@ -61,9 +58,12 @@ class ExperienceManager:
             projectiles_found_in_pair_of_images = self.images_pair_processor.find_projectiles_in_images()
             self.projectiles_coordinates_2d.append(projectiles_found_in_pair_of_images)
 
-        self.projectiles_coordinates_2d = extraction_postprocessing_function(self.projectiles_coordinates_2d)
+        self.projectiles_coordinates_2d = post_detecting_function(self.projectiles_coordinates_2d)
 
     def compute_3d_coords_from_2d_coords_pair(self, pair_coords_2d: List[np.ndarray, np.ndarray]) -> np.ndarray:
+        if not can_be_reconstructed(pair_coords_2d):
+            return []
+
         return cv2.triangulatePoints(
             projMatr1=self.camera_setup.left_camera.get_projection_matrix(),
             projMatr2=self.camera_setup.right_camera.get_projection_matrix(),
@@ -75,3 +75,14 @@ class ExperienceManager:
         for projectile_coords_pair_2d in self.projectiles_coordinates_2d:
             projectile_3d_coords = self.compute_3d_coords_from_2d_coords_pair(projectile_coords_pair_2d)
             self.projectile_trajectory.append(projectile_3d_coords)
+
+
+def can_be_reconstructed(pair_coords_2d: List[np.ndarray, np.ndarray]) -> bool:
+    left_2d_coords = pair_coords_2d[0]
+    right_2d_coords = pair_coords_2d[1]
+
+    valid_shapes = [(2, 1), (2,)]
+
+    if left_2d_coords.shape not in valid_shapes or right_2d_coords.shape not in valid_shapes:
+        return False
+    return True
