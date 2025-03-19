af8db592c4f774f5cea4b746b0b5d9f7faf88c9e
Erwan <mr.bloux@gmail.com>
2025-03-19 11:27:31 +0100

    implemented correctly files manager

--- src/managers/files_manager.py
+++ src/managers/files_manager.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-from typing import List, Union
+from typing import List, Union, Tuple
 
 import cv2 as cv
 import numpy as np
 
 from configuration_reader import Config
 from constants import ALLOWED_IMAGE_FORMATS
-from data_types.data_types import TimedPathPair
+from data_types.data_types import TimedPathPair, Pair
 from src.constants import CameraIdentifier
 
 
@@ -14,14 +14,20 @@ class FilesManager:
     def __init__(self):
         self._directory_images_left_camera: Path = None
         self._directory_images_right_camera: Path = None
+        self._delta_time_per_image: float = None
 
         self.list_timed_matching_image_path_pair: List[TimedPathPair] = []
 
     def update_from_config(self, config: Config):
         self.set_directory_images_left_camera(config.left_camera_config.directory_path)
         self.set_directory_images_right_camera(config.right_camera_config.directory_path)
+        delta_time_per_image = 1/config.left_camera_config.framerate
+        self.set_delta_time_per_image(delta_time_per_image)
         if self.is_valid():
             self.create_list_timed_matching_image_path_pair()
+
+    def set_delta_time_per_image(self, new_delta_time_per_image):
+        self._delta_time_per_image = new_delta_time_per_image
 
     def set_directory_images_left_camera(self, directory: Union[str, Path]):
         if isinstance(directory, str):
@@ -33,7 +39,7 @@ class FilesManager:
             directory = Path(directory)
         self._directory_images_right_camera = directory
 
-    def get_images_name_from_camera(self, camera_identifier: CameraIdentifier):
+    def get_images_paths_from_camera(self, camera_identifier: CameraIdentifier) -> List[str]:
         images_paths = []
         if camera_identifier == CameraIdentifier.LEFT_CAMERA:
             for extension in ALLOWED_IMAGE_FORMATS:
@@ -43,40 +49,39 @@ class FilesManager:
                 images_paths += list(self._directory_images_right_camera.glob(f"*{extension}"))
         return [str(image_path) for image_path in images_paths]
 
-    def create_list_timed_matching_image_path_pair(self, starting_point=None):
+    def create_list_timed_matching_image_path_pair(self, starting_point=None) -> None:
         if starting_point:
             # TODO : we should do something in case we want to process images after a certain point
             pass
 
-        list_images_left_camera_path = self.get_images_name_from_camera(CameraIdentifier.LEFT_CAMERA)
-        list_images_right_camera_path = self.get_images_name_from_camera(CameraIdentifier.RIGHT_CAMERA)
 
-        list_images_left_camera_path.sort()
-        list_images_right_camera_path.sort()
-
-        maximum_number_of_pairs = min(len(list_images_left_camera_path), len(list_images_right_camera_path))
+        list_images_paths_left_camera, list_images_paths_right_camera = self._get_pair_lists_of_sorted_images_path()
+        maximum_number_of_pairs = min(len(list_images_paths_left_camera), len(list_images_paths_right_camera))
         timestamp = 0
-        for i in range(maximum_number_of_pairs):
-            image_left_camera_path = list_images_left_camera_path[i]
-            closest_image_right_camera_path = find_closest_image_path_to_form_pair(
-                image_left_camera_path, list_images_right_camera_path, i
-            )
+        for idx in range(maximum_number_of_pairs):
+            left_image_path = list_images_paths_left_camera[idx]
+            right_image_path = list_images_paths_right_camera[idx]
 
-            # TODO: add a way to know the timestamp (either the camera fps, or the image name)
-            timestamp += 1
+            timed_matching_path_pair = TimedPathPair(timestamp, left_image_path, right_image_path)
+            self.list_timed_matching_image_path_pair.append(timed_matching_path_pair)
 
-            timed_matching_pair = TimedPathPair(timestamp, image_left_camera_path, closest_image_right_camera_path)
-            self.list_timed_matching_image_path_pair.append(timed_matching_pair)
+            timestamp += self._delta_time_per_image
+
+    def _get_pair_lists_of_sorted_images_path(self)-> Tuple:
+
+        list_images_paths_left_camera = self.get_images_paths_from_camera(CameraIdentifier.LEFT_CAMERA)
+        list_images_paths_right_camera = self.get_images_paths_from_camera(CameraIdentifier.RIGHT_CAMERA)
+
+        list_images_paths_left_camera.sort()
+        list_images_paths_right_camera.sort()
+
+        return list_images_paths_left_camera, list_images_paths_right_camera
 
     def is_valid(self):
-        if self._directory_images_left_camera is None or self._directory_images_right_camera is None:
+        if self._directory_images_left_camera is None or self._directory_images_right_camera is None or self._delta_time_per_image is None:
             return False
         return self._directory_images_left_camera.is_dir() and self._directory_images_right_camera.is_dir()
 
-
-def find_closest_image_path_to_form_pair(current_image_name: str, list_image_to_compare, current_image_idx=None) -> str:
-    # TODO: find how to return the closest image to the timestamp.
-    return list_image_to_compare[current_image_idx]
 
 
 def read_image(image_path: Union[str, Path], in_grayscale: bool = False) -> np.ndarray:
